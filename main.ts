acd87f3003defa89b062e79a580b4e14fbb03c00
Rob Wilson <rob@rwilson.org.uk>
2022-04-17 19:27:14 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 let ball = game.createSprite(2, 2)
 ball.turn(Direction.Right, 45)
 basic.forever(function () {
-    ball.move(1)
-    ball.ifOnEdgeBounce()
-    basic.pause(100)
+    for (let index = 0; index <= 4; index++) {
+        ball.move(1)
+        ball.ifOnEdgeBounce()
+        basic.pause(200)
+    }
+    ball.turn(Direction.Right, randint(0, 270))
 })
